c3553220db3eeae201ac64b3e5d340f75fb25cae
alan chen <alantruekimo@yahoo.com.tw>
2013-05-11 16:41:25 +0800

    雙向版

--- chess.js
+++ chess.js
@@ -1,7 +1,8 @@
 var mdx, mdy;
 var drag = false;
 var A, Q, T = true, w;
-var x1, y1, x2, y2, C;
+var x1, y1, x2, y2, C, D;
+
 function init() {
 	A = new Array(9); //宣告棋種屬性分布的陣列
 	Q = new Array(9); //宣告棋子ID分布的陣列
@@ -13,32 +14,17 @@ function init() {
 			Q[i][j] = ""; //ID預設值→空字串
 		}
 	}
-	initChess("b4 ", -4, 0, 0);//車
-	initChess("b5 ", -5, 1, 0);//馬
-	initChess("b3 ", -3, 2, 0);//象
-	initChess("b2 ", -2, 3, 0);//士
-	initChess("b1 ", -1, 4, 0);//將
-	initChess("b22", -2, 5, 0);//士
-	initChess("b33", -3, 6, 0);//象
-	initChess("b55", -5, 7, 0);//馬
-	initChess("b44", -4, 8, 0);//車
-	initChess("b6 ", -6, 1, 2);//包
-	initChess("b66", -6, 7, 2);//包
-	initChess("b71", -7, 0, 3);//卒
-	initChess("b72", -7, 2, 3);//卒
-	initChess("b73", -7, 4, 3);//卒
-	initChess("b74", -7, 6, 3);//卒
-	initChess("b75", -7, 8, 3);//卒
-	initChess("a4 ", 4, 0, 9);//俥
-	initChess("a5 ", 5, 1, 9);//傌
-	initChess("a3 ", 3, 2, 9);//相
-	initChess("a2 ", 2, 3, 9);//仕
-	initChess("a1 ", 1, 4, 9);//帥
+	
+	initChess("a4", 4, 0, 9);//俥
+	initChess("a5", 5, 1, 9);//傌
+	initChess("a3", 3, 2, 9);//相
+	initChess("a2", 2, 3, 9);//仕
+	initChess("a1", 1, 4, 9);//帥
 	initChess("a22", 2, 5, 9);//仕
 	initChess("a33", 3, 6, 9);//相
 	initChess("a55", 5, 7, 9);//傌
 	initChess("a44", 4, 8, 9);//俥
-	initChess("a6 ", 6, 1, 7);//炮
+	initChess("a6", 6, 1, 7);//炮
 	initChess("a66", 6, 7, 7);//炮
 	initChess("a71", 7, 0, 6);//兵
 	initChess("a72", 7, 2, 6);//兵
@@ -46,12 +32,61 @@ function init() {
 	initChess("a74", 7, 6, 6);//兵
 	initChess("a75", 7, 8, 6);//兵
 	
+	initChess("b4", -4, 0, 9);//車
+	initChess("b5", -5, 1, 9);//馬
+	initChess("b3", -3, 2, 9);//象
+	initChess("b2", -2, 3, 9);//士
+	initChess("b1", -1, 4, 9);//將
+	initChess("b22", -2, 5, 9);//士
+	initChess("b33", -3, 6, 9);//象
+	initChess("b55", -5, 7, 9);//馬
+	initChess("b44", -4, 8, 9);//車
+	initChess("b6", -6, 1, 7);//包
+	initChess("b66", -6, 7, 7);//包
+	initChess("b71", -7, 0, 6);//卒
+	initChess("b72", -7, 2, 6);//卒
+	initChess("b73", -7, 4, 6);//卒
+	initChess("b74", -7, 6, 6);//卒
+	initChess("b75", -7, 8, 6);//卒
+	
+	initXXXChess("a4x", 8, 0);//俥
+	initXXXChess("a5x", 7, 0);//傌
+	initXXXChess("a3x", 6, 0);//相
+	initXXXChess("a2x", 5, 0);//仕
+	initXXXChess("a1x", 4, 0);//帥
+	initXXXChess("a22x", 3, 0);//仕
+	initXXXChess("a33x", 2, 0);//相
+	initXXXChess("a55x", 1, 0);//傌
+	initXXXChess("a44x", 0, 0);//俥
+	initXXXChess("a6x", 7, 2);//炮
+	initXXXChess("a66x", 1, 2);//炮
+	initXXXChess("a71x", 8, 3);//兵
+	initXXXChess("a72x", 6, 3);//兵
+	initXXXChess("a73x", 4, 3);//兵
+	initXXXChess("a74x", 2, 3);//兵
+	initXXXChess("a75x", 0, 3);//兵
+	
+	initXXXChess("b4x", 8, 0);//車
+	initXXXChess("b5x", 7, 0);//馬
+	initXXXChess("b3x", 6, 0);//象
+	initXXXChess("b2x", 5, 0);//士
+	initXXXChess("b1x", 4, 0);//將
+	initXXXChess("b22x", 3, 0);//士
+	initXXXChess("b33x", 2, 0);//象
+	initXXXChess("b55x", 1, 0);//馬
+	initXXXChess("b44x", 0, 0);//車
+	initXXXChess("b6x", 7, 2);//包
+	initXXXChess("b66x", 1, 2);//包
+	initXXXChess("b71x", 8, 3);//卒
+	initXXXChess("b72x", 6, 3);//卒
+	initXXXChess("b73x", 4, 3);//卒
+	initXXXChess("b74x", 2, 3);//卒
+	initXXXChess("b75x", 0, 3);//卒
+	
 	//initFog();
 }
 
-function initChess(id, k, x, y) {
-	A[x][y] = k;
-	Q[x][y] = id;
+function initChess(id, k, x, y) {	
 	
 	var newDiv = document.createElement("div");
 	
@@ -68,7 +103,42 @@ function initChess(id, k, x, y) {
 	newDiv.onmousemove = mv;
 	newDiv.onmouseup = mup;
 	
-	document.body.appendChild(newDiv);
+	if ((id.substr(0, 1) == "a" && id.substr(-1, 1) != "x") ||
+		(id.substr(0, 1) == "b" && id.substr(-1, 1) == "x")) {
+		A[x][y] = k;
+		Q[x][y] = id;
+		document.body.appendChild(newDiv);
+	}
+	else {
+		A[8-x][9-y] = k;
+		Q[8-x][9-y] = id;
+		var bg0 = document.getElementById("BG0");
+		bg0.appendChild(newDiv);
+	}
+}
+
+function initXXXChess(id, x, y) {	
+	
+	var newDiv = document.createElement("div");
+	
+	newDiv.id = id;
+	newDiv.style.position = "absolute";
+	newDiv.style.zIndex = 1;
+	newDiv.style.width = "60px";
+	newDiv.style.height = "60px";
+	newDiv.style.backgroundImage = "url(images/" + id.substr(0, 2) + ".png)"; 
+	newDiv.style.backgroundRepeat = "no-repeat";
+	newDiv.style.left = 34 + x *64 + "px";
+	newDiv.style.top = 34 + y * 64 + "px";
+	
+	if ((id.substr(0, 1) == "a" && id.substr(-1, 1) != "x") ||
+		(id.substr(0, 1) == "b" && id.substr(-1, 1) == "x")) {
+		document.body.appendChild(newDiv);
+	}
+	else {
+		var bg0 = document.getElementById("BG0");
+		bg0.appendChild(newDiv);
+	}
 }
 
 function initFog() {	
@@ -95,33 +165,34 @@ function initFog() {
 	}
 }
 
-function md() {
-	C = event.srcElement;//取得觸發事件的物件(棋子)
-	w = C.id.substr(0, 1);//取得代表黑或紅方的關鍵字
+function md(event) {
+	C = event.srcElement ? event.srcElement : event.target; //取得觸發事件的物件(棋子)
+	D = document.getElementById(C.id + "x");
+	w = C.id.substr(0, 1); //取得代表黑或紅方的關鍵字
 	//未輪到正確下棋方時跳出副程式
 	if ((w == "a" && T == false) || (w == "b" && T == true)) return;
-	drag = true;//進入拖曳狀態
-	mdx = event.offsetX;//取得拖曳起點X
-	mdy = event.offsetY;//取得拖曳起點Y
-	C.style.zIndex = 2;//提升拖曳中棋子的層次
-	C.style.cursor = "pointer";//改變游標為手指按棋的圖示
-	x1 = p2g(C.style.posLeft);//換算網頁像素點座標為棋盤座標X
-	y1 = p2g(C.style.posTop);//換算網頁像素點座標為棋盤座標Y
+	drag = true; //進入拖曳狀態
+	mdx = event.offsetX; //取得拖曳起點X
+	mdy = event.offsetY; //取得拖曳起點Y
+	C.style.zIndex = 2; //提升拖曳中棋子的層次
+	C.style.cursor = "pointer"; //改變游標為手指按棋的圖示
+	x1 = p2g(C.style.posLeft); //換算網頁像素點座標為棋盤座標X
+	y1 = p2g(C.style.posTop); //換算網頁像素點座標為棋盤座標Y
 }
 
-function mv() {
+function mv(event) {
 	if (drag) {//如果是拖曳狀態
-		C.style.posLeft += event.offsetX - mdx;//X方向移動
-		C.style.posTop += event.offsetY - mdy;//Y方向移動
+		C.style.posLeft += event.offsetX - mdx; //X方向移動
+		C.style.posTop += event.offsetY - mdy; //Y方向移動
 	}
 }
 
 function mup() {
 	if (drag) {//如果是拖曳狀態
-		drag = false;//結束拖曳狀態
-		C.style.cursor = "default";//恢復正常游標
-		C.style.zIndex = 1;//恢復正常層次
-		chess(C);//檢視下棋動作合法性
+		drag = false; //結束拖曳狀態
+		C.style.cursor = "default"; //恢復正常游標
+		C.style.zIndex = 1; //恢復正常層次
+		chess(C); //檢視下棋動作合法性
 	}
 }
 
@@ -142,47 +213,113 @@ function chess(C) {//下棋合法性檢查
 	C.style.posTop = Math.round(y / 64) * 64 - 30; //移動至棋格垂直位置
 	x2 = p2g(C.style.posLeft); //取得棋子移至的棋格座標X
 	y2 = p2g(C.style.posTop); //取得棋子移至的棋格座標Y
+	
+	if (w == "b") {
+		x1 = 8 - x1;
+		y1 = 9 - y1;
+		x2 = 8 - x2;
+		y2 = 9 - y2;
+	}
+	
 	if (outside() || occupy()) {//超出棋盤或有我方棋子
+		if (w == "b") {
+			x1 = 8 - x1;
+			y1 = 9 - y1;
+		}
 		C.style.posLeft = g2p(x1); //跳回原處x1
 		C.style.posTop = g2p(y1); //跳回原處y1
 		return;
 	}
-	var k = A[x1][y1];//取得棋種
-	var legal = false;//預設非法
+	
+	var k = A[x1][y1];
+	var legal = false;
 	switch (k) {
-		case 1: legal = onestep() && inpalace(); break;//帥
-		case -1: legal = onestep() && inpalace(); break;//將                
-		case 2: legal = dgn(1) && inpalace(); break;//仕
-		case -2: legal = dgn(1) && inpalace(); break;//士
-		case 3: legal = dgn(2) && (y2 >= 5) && freeElephant(); break;//相
-		case -3: legal = dgn(2) && (y2 <= 4) && freeElephant(); break;//象
-		case 4: legal = straight() && between() == 0; break;//俥
-		case -4: legal = straight() && between() == 0; break;//車
-		case 5: legal = horsestep() && freeHorse(); break;//傌
-		case -5: legal = horsestep() && freeHorse(); break;//馬
-		case 6: legal = straight() && cannonStep(); break;//炮
-		case -6: legal = straight() && cannonStep(); break;//包
-		case 7: legal = onestep() && soldier(1); break;//兵
-		case -7: legal = onestep() && soldier(-1); break;//卒
+		case 1: //帥
+			legal = onestep() && inpalace();
+			break;
+		case -1: //將
+			legal = onestep() && inpalace();
+			break;
+		case 2: //仕
+			legal = dgn(1) && inpalace();
+			break;
+		case -2: //士
+			legal = dgn(1) && inpalace();
+			break;
+		case 3: //相
+			legal = dgn(2) && (y2 >= 5) && freeElephant();
+			break;
+		case -3: //象
+			legal = dgn(2) && (y2 <= 4) && freeElephant();
+			break;
+		case 4: //俥
+			legal = straight() && between() == 0;
+			break;
+		case -4: //車
+			legal = straight() && between() == 0;
+			break;
+		case 5: //傌
+			legal = horsestep() && freeHorse();
+			break;
+		case -5: //馬
+			legal = horsestep() && freeHorse();
+			break;
+		case 6: //炮
+			legal = straight() && cannonStep();
+			break;
+		case -6: //包
+			legal = straight() && cannonStep();
+			break;
+		case 7: //兵
+			legal = onestep() && soldier(1);
+			break;
+		case -7: //卒
+			legal = onestep() && soldier(-1);
+			break;
 	}
+	
 	if (legal) { //如果合法
 		if (A[x2][y2] != 0) { //目的地有對方棋子→吃棋
 			var K = document.getElementById(Q[x2][y2]);
 			K.style.visibility = "hidden"; //隱藏對方被吃的棋子
+			var KK = document.getElementById(Q[x2][y2] + "x");
+			KK.style.visibility = "hidden";//隱藏對方被吃的棋子
+			var qq = K.id.substr(1, 1);//將帥被吃了
+			if (qq == "1") reset();//重玩囉
 		}
+		
 		A[x2][y2] = A[x1][y1]; //更新棋種陣列資訊
 		Q[x2][y2] = Q[x1][y1]; //更新物件名稱資訊
 		A[x1][y1] = 0; //清除原位置資訊
 		Q[x1][y1] = ""; //清除原位置資訊
+		
+		if (w == "b") {
+			x2 = 8 - x2;
+			y2 = 9 - y2;
+		}
+		
+		D.style.posLeft = g2p(8 - x2);//對手棋盤棋子移動X
+		D.style.posTop = g2p(9 - y2);//對手棋盤棋子移動Y
+		
 		if (w == "a") { //紅方完成下棋
 			T = false; //換黑棋下
-			BG.title = "輪到黑棋下"; //顯示輪替訊息
+			//msg.innerHTML = "輪到黑棋下→"; //顯示輪替訊息
+			//BG.style.borderColor = "white";//邊框變化
+			//BG0.style.borderColor = "red";//邊框變化
 		}
+		
 		if (w == "b") { //黑方完成下棋
 			T = true; //換紅棋下
-			BG.title = "輪到紅棋下";  //顯示輪替訊息
+			//msg.innerHTML = "←輪到紅棋下";  //顯示輪替訊息
+			//BG0.style.borderColor = "white";//邊框變化
+			//BG.style.borderColor = "red";//邊框變化
+		}		
+	} 
+	else { //如果不合法
+		if (w == "b") {
+			x1 = 8 - x1;
+			y1 = 9 - y1;
 		}
-	} else { //如果不合法
 		C.style.posLeft = g2p(x1); //跳回原位x1
 		C.style.posTop = g2p(y1); //跳回原位y1
 	}
@@ -197,6 +334,7 @@ function outside() {//超出棋盤
 	if (x2 > 8 || x2 < 0 || y2 > 9 || y2 < 0) return true;
 	return false;
 }
+
 function occupy() {//棋格有我方棋子
 	if (A[x1][y1] * A[x2][y2] > 0) return true;
 	return false;
@@ -206,7 +344,6 @@ function onestep() {//移動一格
 	if (Math.abs(x1 - x2) + Math.abs(y1 - y2) == 1) return true;
 	return false;
 }
-
 function inpalace() {//將帥士仕是否在宮中
 	if (x2 > 5 || x2 < 3) return false;
 	if (w == "a" && y2 < 7) return false;
@@ -218,7 +355,6 @@ function dgn(s) {//斜走s格
 	if (Math.abs(x1 - x2) == s && Math.abs(y1 - y2) == s) return true;
 	return false;
 }
-
 function freeElephant() {//卡象腳
 	var xx = Math.round((x1 + x2) / 2);
 	var yy = Math.round((y1 + y2) / 2);
@@ -262,6 +398,7 @@ function freeHorse() {//拐馬腳的規則
 	if (y2 - y1 == 2 && A[x1][y1 + 1] != 0) return false;
 	return true;
 }
+
 function cannonStep() {//炮與包的規則
 	var btw = between();
 	if (btw == 0 && A[x2][y2] == 0) return true;
@@ -274,7 +411,8 @@ function soldier(k) {//兵卒的規則
 		if (y2 - y1 > 0) return false;
 		if (y1 > 4 && y2 == y1) return false;
 		return true;
-	} else {
+	} 
+	else {
 		if (y1 - y2 > 0) return false;
 		if (y1 < 5 && y2 == y1) return false;
 		return true;
